b63c5d23971e2b976a11781facd6c1549a286083
kat-star <katherinewkwong@gmail.com>
2019-10-29 16:17:55 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    addItem();
+  });
+
+function addItem() {
+  const toDoList = document.getElementById('create-task-form');
+
+  toDoList.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const userItem = event.target.querySelector('#new-task-description');
+    
+    const listTasks = document.querySelector('#tasks');
+    const newItem = document.createElement('li');
+    newItem.textContent = userItem.value;
+
+    userItem.value = "";
+    userItem.focus();
+
+    const button = document.createElement('button');
+    button.innerHTML = 'X'
+    button.dataset.description = newItem.textContent
+    newItem.appendChild(button);
+
+    listTasks.appendChild(newItem);
+    button.addEventListener('click', function(event) {
+      event.target.parentElement.remove();
+    })
+    
+  
+  
+
+  });
+}
+
+// function deleteItem() {
+  
+// }
+
+
